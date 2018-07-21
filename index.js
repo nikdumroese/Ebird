2cef6a40ad6f43b49537e815430b819e8b96b1ee
nikdumroese <niklaas.dumroese@gmail.com>
2018-07-21 16:18:16 +0200

    successful API call

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const auth = require('./auth.json');
 const token = auth.token
@@ -10,10 +11,32 @@ app.use(express.static(__dirname + `/public`));
 app.get('/a', function(req, res) {
     // (res.sendFile('index.html'));
     console.log("at get route /");
-    console.log("token: ", token);
 });
 
 
+app.get ('/idaho', function (req, res) {
+  console.log("at idaho get route");
+
+//api reqeust//
+  const axiosConfig = {
+    headers: {
+      "x-ebirdapitoken": token,
+      "content-type": "application/json"
+    }
+  };
+
+  axios.get('https://ebird.org/ws2.0/data/obs/US-ID/recent', axiosConfig)
+    .then(function(response){
+      console.log('**************my response ', response);
+    })
+    .catch(function(error){
+      console.log('*************ERROR ', error);
+    })
 
 
-app.listen(8081, () => console.log('I\'m listening on port 8080'));
+
+
+
+});
+
+app.listen(8080, () => console.log('I\'m listening on port 8080'));
